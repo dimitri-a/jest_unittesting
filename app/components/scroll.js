77acbf97147500602e692d05dfc2bf92afee3149
dimitric <couwenberg@gmail.com>
2017-11-18 20:07:30 +1100

    how to scroll to the next 20 names?

--- app/components/scroll.js
+++ app/components/scroll.js
@@ -2,39 +2,35 @@ import React, {Component} from 'react';
 
 import InfiniteScroll from 'react-infinite-scroller';
 
-import {hamletScripts} from '../json/hamlet'
+import {names} from '../json/names'
 
 export default class Scroll extends Component {
 
     constructor(props) {
         super();
         this.props = props;
-        this.state = {items: hamletScripts, viewItems: []}
-        this.loadItems();
+        this.state = {items: names, viewItems: [], start: 0}
+       this.loadItems();
     }
 
     loadItems = () => {
-        this.state.viewItems = this.state.items.slice(1, 3)
+        this.state.viewItems = this.state.items.slice(this.state.start, this.state.start + 20)
     }
 
     render() {
         return (
             <InfiniteScroll
-                pageStart={0}
                 loadMore={this.loadItems}
                 hasMore={true}
             >
-
                 <ul>
-
-                        {this.state.viewItems.map
-                                            (
-                                                (item) => (
-                                                    <li> {item}!</li>
-                                                )
-                                            )
-                        }
-
+                    {this.state.viewItems.map
+                    (
+                        (item) => (
+                            <li> {item}!</li>
+                        )
+                    )
+                    }
                 </ul>
             </InfiniteScroll>
         );
