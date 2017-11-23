2f56a9750486cdc0d13c7c50063cc7471bc796ce
dimitric <couwenberg@gmail.com>
2017-11-23 16:50:51 +1100

    update

--- app/components/scroll.js
+++ app/components/scroll.js
@@ -1,6 +1,27 @@
 import React, {Component} from 'react';
 
 class Scroll extends Component {
+
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {position: 0, pageSize: 20, displayedNames: this.props.names.slice(0, 20)}
+    }
+
+    next = ()=> {
+
+       // console.log(this);
+        this.setState({position: this.state.position + this.state.pageSize},
+        () => {
+            console.log('this.state.position=',this.state.position);
+            console.log('this.state.pageSize=',this.state.pageSize);
+            //console.log('this.state.pageSize=',this.state.pageSize);
+            console.log('this.props.names=',this.props.names);
+            this.setState({displayedNames: this.props.names.slice(this.state.position,this.state.position+this.state.pageSize)});
+        });
+
+    }
+
     render() {
         let display = this.props.names.map
         (
@@ -11,13 +32,11 @@ class Scroll extends Component {
 
         return (
             <div id='container'>
+                <div className="scroll">
+                    {this.state.displayedNames}
+                </div>
 
-
-            <div className="scroll">
-
-                {display}
-
-            </div>
+                <button onClick={this.next}>next 20</button>
             </div>
         );
     }
